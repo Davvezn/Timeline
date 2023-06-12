3c0db9745af9c59cc04d2c5fee97ddd839e081c0
Davvezn <david.liljequist@elev.ga.ntig.se>
2023-06-12 14:32:12 +0200

    update1

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function showCard(cardIndex) {
     // Hide all cards
-    var cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
+    let cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
       cards[i].style.display = "none";
     }
   
     // Show the selected card
-    var selectedCard = document.getElementById("card-" + (cardIndex + 1));
+    let selectedCard = document.getElementById("card-" + (cardIndex + 1));
     selectedCard.style.display = "block";
   }
+
+  let currentCardIndex = 0; // Track the currently displayed card index
+
+function showCard(cardIndex) {
+    // Hide the currently displayed card
+    let currentCard = document.getElementById("card-" + (currentCardIndex + 1));
+    currentCard.style.display = "none";
+  
+    // Show the selected card
+    let selectedCard = document.getElementById("card-" + (cardIndex + 1));
+    selectedCard.style.display = "block";
+  
+    currentCardIndex = cardIndex; // Update the currently displayed card index
+}
+  
   
